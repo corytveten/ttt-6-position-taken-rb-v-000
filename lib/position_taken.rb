4b2b97f5e288b07161e7004d2ce7bdfbf4a75e56
corytveten <corytveten@gmail.com>
2018-04-09 15:19:59 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,20 @@
 def position_taken?(board, index)
   if board[0] == " "
     return false
-
+    if board[1] == " "
+      return false
+      if board[2] == " "
+        return false
+        if board[3] == " "
+          return false
+          if board[4] == " "
+            return false
+            if board[5] == " "
+              return false
+              if board[6] == " "
+                return false
+                if board[7] == " "
+                  return false
+                  if board[8] == " "
+                    return false
 end
