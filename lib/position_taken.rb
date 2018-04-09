012b4bd2fa0265d60907d139956ba6249b1fe52e
corytveten <corytveten@gmail.com>
2018-04-09 15:34:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,41 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-if board[0] == "X" || "O"
+if board[index] == "X" || "O"
     return true
-  elsif board[1] == "X" || "O"
-      return true
-    elsif board[2] == "X" || "O"
-        return true
-      elsif board[3] == "X" || "O"
-          return true
-        elsif board[4] == "X" || "O"
-            return true
-          elsif board[5] == "X" || "O"
-              return true
-            elsif board[6] == "X" || "O"
-                return true
-              elsif board[7] == "X" || "O"
-                  return true
-                elsif board[8] == "X" || "O"
-                    return true
                   end
-  if board[0] == " "
+  if board[index] == " "
     return false
-  elsif board[1] == " "
-      return false
-    elsif board[2] == " "
-        return false
-      elsif board[3] == " "
-          return false
-        elsif board[4] == " "
-            return false
-          elsif board[5] == " "
-              return false
-            elsif board[6] == " "
-                return false
-              elsif board[7] == " "
-                  return false
-                elsif board[8] == " "
-                    return false
                                   end
 end
