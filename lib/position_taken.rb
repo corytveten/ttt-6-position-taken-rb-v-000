0d2ba38771617ff64656977da09e1d3f736b503b
corytveten <corytveten@gmail.com>
2018-04-09 15:25:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -19,4 +19,23 @@ def position_taken?(board, index)
                 elsif board[8] == " "
                     return false
                   end
+                if board[0] == "X" || "O"
+                    return true
+                  elsif board[1] == "X" || "O"
+                      return true
+                    elsif board[2] == "X" || "O"
+                        return true
+                      elsif board[3] == "X" || "O"
+                          return true
+                        elsif board[4] == "X" || "O"
+                            return true
+                          elsif board[5] == "X" || "O"
+                              return true
+                            elsif board[6] == "X" || "O"
+                                return true
+                              elsif board[7] == "X" || "O"
+                                  return true
+                                elsif board[8] == "X" || "O"
+                                    return true
+                                  end
 end
